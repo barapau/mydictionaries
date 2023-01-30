3d59af39b1cdc6b45390af8e6777bc91a0b846c3
Paula <paula_baranano1@baylor.edu>
2023-01-30 14:11:28 -0600

    class work

--- 1_dictionary start file.py
+++ 1_dictionary start file.py
@@ -1,33 +1,39 @@
 import random
 
+phonebook = {}
 phonebook = {'Chris':'555−1111',
              'Katie':'555−2222',
-             'Joanne':'555−3333'}
+             'Joanne':'555−3333'} #3 elements 
+mydictionary = dict(m=8, n=9)
+print(mydictionary)
+
+#print(f"Number of key-value pairs:" {len(phonebook))}
+
+
+#print()
+#print('*****  start section 1 - print dictionary ********')
+#print()
 
 
 
-print()
-print('*****  start section 1 - print dictionary ********')
-print()
 
 
+#print()
+#print('*****  end section 1 ********')
+#print()
 
 
-
-print()
-print('*****  end section 1 ********')
-print()
-
-
-'''
 
 
 print()
 print('*****  start section 2 - search dictionary ********')
 print()
 
-
-
+name = 'Chris'
+if name in phonebook:
+    print(phonebook[name]) #expected to sethe phone number, which is the value of that key  
+else:
+    print(f"{name} does not exist in the phonebook")
 
 
 
@@ -40,13 +46,14 @@ print()
 
 
 
-
-
 print()
 print('*****  start section 3 - edit/append dictionary ********')
 print()
 
-
+print(phonebook)
+phonebook['Chris'] = '555-4444' #update Chris phone number 
+phonebook['Joe'] = '555-0123'  #if joe doesnt exist, its  going to append to the end of the dictionary
+print(phonebook)
 
 
 
@@ -58,12 +65,14 @@ print()
 
 
 
-
 print()
 print('*****  start section 4 - delete/remove from dictionary ********')
 print()
 
-
+print(phonebook)
+del phonebook['Chris']
+print(phonebook)
+#if the key does not exist, the delete comand will give you an error 
 
 
 print()
@@ -80,6 +89,17 @@ print('*****  start section 5 - iterate through keys, values, items ********')
 print()
 
 
+for key in phonebook:
+    print(f"The key is: {key} and the value is {phonebook(key)}")
+
+for value in phonebook.values():
+    print(value)
+
+for k,v in phonebook.iteam():
+    print()
+
+for ph_tuple in phonebook.items():
+    print(ph_tuple)
 
 
 
@@ -96,9 +116,12 @@ print()
 print('*****  start section 6 - using get and clear ********')
 print()
 
+name = "Chris"
+phone = phonebook.get(name, '555-9999')
+print(phone)
 
-
-
+phonebook.clear()
+print(phonebook)  #object was not deleted but it clears out the content 
 
 
 print()
@@ -111,7 +134,9 @@ print()
 print('*****  start section 7 - using pop method ********')
 print()
 
-
+remove = phonebook.pop('Chris',  'not found')
+print(remove)
+print(phonebook)
 
 
 
@@ -127,7 +152,10 @@ print('*****  start section 8 - using popitem ********')
 print()
 
 
-
+a = phonebook.popItem()
+print(a)
+print(phonebook)
+#Joan is popping out because is the last item = it suppose to be random 
 
 
 
@@ -142,16 +170,19 @@ print('*****  start section 9 - using random and converting to list ********')
 print()
 
 
+list_of_keys = list(phonebook)
+random_key = random.choice(list_of_keys)
+print(random_key)
+print(phonebook[random_key])
+
+#without using variables and in one line
+print(phonebook[random.choice])(list(phonebook))
 
 
 
 print()
 print('*****  end section 9 ********')
 print()
-
-
-'''
-
 
 
 
--- 2_person_dict.py
+++ 2_person_dict.py
@@ -3,5 +3,28 @@ person["fname"] = "Joe"
 person["lname"] = "Fonebone"
 person["age"] = 51
 person["spouse"] = "Edna"
-person["children"] = ["Ralph", "Betty", "Joey"]
-person["pets"] = {"dog": "Fido", "cat": "Sox"}
+person["children"] = ["Ralph", "Betty", "Joey"] #value is a list
+person["pets"] = {"dog": "Fido", "cat": "Sox"} #value is another dictionary
+
+print(person)
+
+#name of  the second child
+print(person.get("children")[1]) #doesnt give error if it doesnt  exist 
+print(person["children"][1]) #best solution
+
+print(type(person["children"])) #which kind of element im dealing with 
+
+
+#the name of the cat
+print(type(person["pets"]))
+print(person["pets"]["cat"])
+
+#use a  for loop to list each child
+for i in person["children"]:
+    print(i)
+
+#print out  the pets in this format
+#'Type of pet: dog name of pet: Fio 
+for i,j in  person['pets'].items():
+    print(f"Type of pet:{i} name of pet: {j}")
+#the key being i and the  j being value  
