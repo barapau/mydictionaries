01fb31f60708469d4b45dd7e8636dd51c826a946
Paula <paula_baranano1@baylor.edu>
2023-05-04 20:03:20 -0500

    hmw

--- extra_credit.py
+++ extra_credit.py
@@ -5,3 +5,11 @@
 #- 'Father', 'God', 'Christ', 'Spirit', 'life', 'man' (case sensitive). 
 #The text file is available here (note: all punctuation marks, chapter and verse references have been removed to allow to analyze each word) - 
 
+infile = open('BookJohn.txt', 'r')
+john = infile.read()
+words_to_count = ['Father', 'God', 'Christ', 'Spirit', 'life', 'man']
+
+word_counts = {word: john.count(word) for word in words_to_count}
+
+for word, count in word_counts.items():
+    print(word, count)
